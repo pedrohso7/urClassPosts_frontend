20f360f4e52e12da350af37076fcc283118ca7bb
Pedro Oliveira <pedro.oliveira3@ufv.br>
2020-12-09 12:12:27 -0300

    fixing errors

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import firebase from 'firebase';
 import Routes from './routes'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './global.css'
-
-// Your web app's Firebase configuration
-      // For Firebase JS SDK v7.20.0 and later, measurementId is optional
       
+const firebaseConfig = {
+  apiKey: "AIzaSyDaWaI9ry5vMYdA962hpMISIFw2oiWq2jA",
+  authDomain: "urclassposts.firebaseapp.com",
+  databaseURL: "https://urclassposts-default-rtdb.firebaseio.com/",
+  projectId: "urclassposts",
+  storageBucket: "urclassposts.appspot.com",
+  messagingSenderId: "631514982159",
+  appId: "1:631514982159:web:112eb6174639010ddd807a",
+  measurementId: "G-SY3MVFLS8Q"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 function App() {
-   
   return (
     <div>
       <Routes/>
